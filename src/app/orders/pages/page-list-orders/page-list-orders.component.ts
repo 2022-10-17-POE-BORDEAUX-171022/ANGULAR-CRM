641923e71126ffce5efc2c31f8fee94274e214c5
dnyuhm <dylanmeuriot@gmail.com>
2023-01-05 11:30:19 +0100

    Commit 27 : Pipe total & rxjs filter Unsubscribe & pipe async

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Order } from 'src/app/core/models/order';
 import { OrdersService } from 'src/app/core/services/orders.service';
 
@@ -8,21 +9,19 @@ import { OrdersService } from 'src/app/core/services/orders.service';
   styleUrls: ['./page-list-orders.component.scss'],
 })
 export class PageListOrdersComponent {
+  public collection$: Observable<Order[]>;
   public headers: string[];
-  public collection!: Order[];
 
   constructor(private ordersService: OrdersService) {
+    this.collection$ = this.ordersService.collection$;
     this.headers = [
       'Type',
       'Client',
       'Jours',
       'Tjm',
-      'total HT',
+      'Total HT',
       'Total TTC',
       'Status',
     ];
-    this.ordersService.collection$.subscribe((data) => {
-      this.collection = data;
-    });
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { GabaritsModule } from '../gabarits/gabarits.module';
 import { IconsModule } from '../icons/icons.module';
-import { TableLightComponent } from './components/table-light/table-light.component';
 import { BtnComponent } from './components/btn/btn.component';
+import { TableLightComponent } from './components/table-light/table-light.component';
+import { TotalPipe } from './pipes/total.pipe';
 
 @NgModule({
-  declarations: [
-    TableLightComponent,
-    BtnComponent
-  ],
+  declarations: [TableLightComponent, BtnComponent, TotalPipe],
   imports: [CommonModule],
-  exports: [GabaritsModule, IconsModule, TableLightComponent, BtnComponent],
+  exports: [
+    GabaritsModule,
+    IconsModule,
+    TableLightComponent,
+    BtnComponent,
+    TotalPipe,
+  ],
 })
 export class SharedModule {}
