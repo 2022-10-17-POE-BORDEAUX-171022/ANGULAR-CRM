ec3b29474db5616a6badcb5cdd0909de0abd45dd
dnyuhm <dylanmeuriot@gmail.com>
2023-01-09 10:08:14 +0100

    Edit order done

--- src/app/orders/pages/page-edit-order/page-edit-order.component.ts
+++ src/app/orders/pages/page-edit-order/page-edit-order.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
 import { StateOrder } from 'src/app/core/enums/state-order';
+import { Order } from 'src/app/core/models/order';
+import { OrdersService } from 'src/app/core/services/orders.service';
 
 @Component({
   selector: 'app-page-edit-order',
@@ -10,21 +14,36 @@ import { StateOrder } from 'src/app/core/enums/state-order';
 export class PageEditOrderComponent {
   public states: string[];
   public form!: FormGroup;
+  public id: number;
+  public item$: Observable<Order>;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private ordersService: OrdersService,
+    private router: Router
+  ) {
     this.states = Object.values(StateOrder);
-
-    // this.form = this.formBuilder.group({
-    //   prestaType: [this.order.prestaType],
-    //   client: [this.order.client],
-    //   nbDays: [this.order.nbDays],
-    //   unitPrice: [this.order.unitPrice],
-    //   vat: [this.order.vat],
-    //   state: [this.order.state],
-    //   comment: [this.order.comment],
-    //   id: [this.order.id],
-    // });
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.item$ = this.ordersService.getItemById(this.id).pipe(
+      tap((order) => {
+        this.form = this.formBuilder.group({
+          prestaType: [order.prestaType],
+          client: [order.client],
+          nbDays: [order.nbDays],
+          unitPrice: [order.unitPrice],
+          vat: [order.vat],
+          state: [order.state],
+          comment: [order.comment],
+          id: [order.id],
+        });
+      })
+    );
   }
 
-  public onSubmit(): void {}
+  public onSubmit(): void {
+    this.ordersService.update(this.form.value).subscribe(() => {
+      this.router.navigate(['orders']);
+    });
+  }
 }
